389ab912924070fd3990600a75365e2c714a3655
Manu Kumar <manu@neargroup.me>
2021-02-04 14:31:30 +0530

    Initial Commit

---
+++ TwitterLogin/TwitterLogin.h
@@ -0,0 +1,18 @@
+//
+//  TwitterLogin.h
+//  TwitterLogin
+//
+//  Created by Manu Singh on 04/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TwitterLogin.
+FOUNDATION_EXPORT double TwitterLoginVersionNumber;
+
+//! Project version string for TwitterLogin.
+FOUNDATION_EXPORT const unsigned char TwitterLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitterLogin/PublicHeader.h>
+
+
